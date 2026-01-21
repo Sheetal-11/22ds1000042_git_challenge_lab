04c42f18341b79fb0ba2624364f003e65f2a55ef
22ds1000042 <22ds1000042@ds.study.iitm.ac.in>
2026-01-21 21:57:56 +0530

    Completed the calculator.py code

--- main.py
+++ main.py
@@ -17,9 +17,42 @@ from multiply import multiply
 # 5. Add a division feature to the calculator
 from divide import division
 
+operations = {
+        "+" : add,
+        "-" : subtract,
+        "*" : multiply,
+        "/" : division,
+        }
 
-def calculator ():
 
-    pass
+def calculator():
+    
+    print(logo)
+    
+    num1 = float( input("What's the first number?: "))
+    
+    end_calculation = False
+    
+    while not end_calculation:
+        
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation: ")
+        num_2 = float( input("What's the next number?: "))
+            
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num_2)
+    
+        print(f"{num1} {operation_symbol} {num_2} = {answer}")
+            
+        should_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ").lower()
+        
+        if should_continue == 'y':
+            num1 = answer
+            
+        if should_continue == 'n':
+            end_calculation = True
+            calculator()
+
 
 calculator()
